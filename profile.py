c33ea1bf48f345f2f6152bd7485f35566791dfed
repopreeth <repopreeth@users.noreply.github.com>
2014-10-20 01:13:24 -0700

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,26 @@
+import tweepy
+import access
+
+auth = tweepy.OAuthHandler(access.consumer_key, access.consumer_secret)
+auth.set_access_token(access.access_token, access.access_token_secret)
+
+
+class Profile:
+
+
+
+    def __init__(self, user):
+        self.user = user
+
+    def getTweets(self):
+        twitter = tweepy.API(auth)
+        user = twitter.get_user(self.user)
+        tweets = twitter.user_timeline(self.user, count=100)
+        return tweets
+
+#        for tweet in public_tweets:
+#            print tweet.text.encode('utf8')
+
+    def getProfile(self):
+        #TODO get profile
+        return
